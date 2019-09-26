2d35a2e2bc2fe29737b9abd9b265bedd70f1775c
Taylor <taylor.winter@vuw.ac.nz>
2019-09-26 21:37:13 +1200

    Corrected layout of effect builder

--- uiTabs/modelTab.R
+++ uiTabs/modelTab.R
@@ -1,5 +1,37 @@
 modelTab <- tabItem(tabName = "model",
-                      fluidRow(
-                        h1("Im a model tab !")
+                    # This fluid row contains the effect builder and plot boxes
+                    fluidRow(
+                      # Effect builder
+                      box(
+                        status = "primary",
+                        title = "Effect builder",
+                        fluidRow(
+                            # Still figuring out what UI element will be used to capture effects, a combination of dropdowns with a text stem updated to the left of it ?
+                          box(
+                            width=12,
+                            "This is where you will build effects"
+                            )
+                          ),
+                        fluidRow(
+                            # This will need to be a selectizeInput below and the selected options get populated as effects get added
+                          box(
+                            width=12,
+                            selectizeInput("effects",
+                                           "Effects in model",
+                                           choices = c("M mediates X and Y",
+                                                       "Y is the outcome"),
+                                           multiple=TRUE,
+                                           options=list(plugins=list('drag_drop','remove_button')))
+                            )
+                          ) 
+                        ),
+                      
+                      # Plot box and lavaan code  
+                      tabBox(
+                        title = "Model",
+                        # I need to add code for the panel IDs so we can parse info to server
+                        tabPanel("Plot", "First tab content"),
+                        tabPanel("Code", "Tab content 2")
+                        )
                       )
-)
+                    )
